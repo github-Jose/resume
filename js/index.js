fdf329963859662253c08b3ee4eb700fedb834ce
Jose <2648413936@qq.com>
2018-11-01 23:46:51 +0800

    skills and portfolio

--- js/index.js
+++ js/index.js
@@ -1,15 +1,37 @@
 window.onload=function(){
+    var scrollSite = document.documentElement.scrollTop || document.body.scrollTop;
+    if(scrollSite == 0){
+        console.log(scrollSite);
+        // console.log(topNavBar);
+        topNavBar.style.backgroundColor = 'transparent';
+    }else{
+        topNavBar.style.backgroundColor = '#fff';
+    }
     window.onscroll =function (){
         var scrollSite = document.documentElement.scrollTop || document.body.scrollTop;
         var topNavBar = document.getElementById('topNavBar');
         var list = document.getElementById('list');
     // console.log(scrollSite)
         if(scrollSite == 0){
-            console.log('顶部');
+            console.log(scrollSite);
             // console.log(topNavBar);
-            topNavBar.style.backgroundColor = '#000';
+            topNavBar.style.backgroundColor = 'transparent';
         }else{
             topNavBar.style.backgroundColor = '#fff';
         }
+    }
+    var portfolioAll = document.getElementById('portfolioAll'); 
+    var portfolioFramke = document.getElementById('portfolioFramke'); 
+    var portfolioNative = document.getElementById('portfolioNative'); 
+    var barInner = document.getElementById('barInner');
+    portfolioAll.onclick = function(){
+        console.log('1');
+        barInner.className = 'bar-inner bar-stage1'
+    }
+    portfolioFramke.onclick = function(){
+        barInner.className = 'bar-inner bar-stage2'
+    }
+    portfolioNative.onclick = function(){
+        barInner.className = 'bar-inner bar-stage3'
     }
 }
